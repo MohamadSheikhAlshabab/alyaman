e60b97a527970639107654ef8de45daebad7b154
Mohamad Sheikh Alshabab <alshabab.moh@gmail.com>
2022-08-12 03:07:19 +0300

    omit lazy loading from slider's images to lower down LCP

--- index.js
+++ index.js
@@ -98,46 +98,6 @@ function showSlides1() {
   setTimeout(showSlides1, 5000); 
 }
 
-// <!-- email validtion-->
-
-const email = document.querySelector("#email");
-const icon1 = document.querySelector(".icon1");
-const icon2 = document.querySelector(".icon2");
-const error = document.querySelector(".error-text");
-const btn = document.querySelector("button");
-let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-function check(){
-  if(email.value.match(regExp)){
-    email.style.borderColor = "#27ae60";
-    email.style.background = "#eafaf1";
-    email.style.outline="#F60419";
-    email.style.borderColor="#F60419"
-    icon1.style.display = "none";
-    icon2.style.display = "block";
-    error.style.display = "none";
-    btn.style.display = "block";
-  }else{
-    email.style.borderColor = "#e74c3c";
-    email.style.background = "#fceae9";
-    email.style.outline="#F60419";
-    email.style.borderColor="#F60419"
-    icon1.style.display = "block";
-    icon2.style.display = "none";
-    error.style.display = "block";
-    btn.style.display = "none";
-  }
-  if(email.value == ""){
-    email.style.borderColor = "lightgrey";
-    email.style.background = "#fff";
-    email.style.outline="#F60419";
-    email.style.borderColor="#F60419"
-    icon1.style.display = "none";
-    icon2.style.display = "none";
-    error.style.display = "none";
-    btn.style.display = "none";
-  }
-}
-
 // carousel images
 
 let carouselItems=null;
